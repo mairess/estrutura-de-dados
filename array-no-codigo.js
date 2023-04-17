83a7f5045ac6a340decb359b5c99cb26135cb6ee
mairess <trabalho.maires@gmail.com>
2023-04-17 11:49:07 -0300

    create array-no-codigo.js

---
+++ array-no-codigo.js
@@ -0,0 +1,25 @@
+const pilotos = ['Senna', 'Prost', 'Schumacher', 'Hamilton']
+
+// a indexação (inde) começa pelo número 0
+console.log(pilotos[0]) // Senna
+console.log(pilotos[3]) // Prost
+
+// acessar o tamnaho do array
+console.log(pilotos.length)
+
+//iterável
+for (let piloto of pilotos){
+    console.log(piloto)
+}
+
+// adicionar elemento
+pilotos.push('Alonso')
+console.log(pilotos)
+
+// encontra um elemento
+const senna =  pilotos.find(piloto => piloto === 'senna')
+console.log("senna")
+
+// deletar elemento
+pilotos.splice(1, 1)
+console.log(pilotos)
